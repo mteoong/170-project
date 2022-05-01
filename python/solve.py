67577e0772815ff16f2cef17f10462be7991cbfe
Charles He <cyhe@berkeley.edu>
2022-05-01 15:27:48 -0700

    setup for project

--- python/solve.py
+++ python/solve.py
@@ -13,6 +13,10 @@ from instance import Instance
 from solution import Solution
 from file_wrappers import StdinFileWrapper, StdoutFileWrapper
 
+#my imports
+from itertools import chain, combinations
+import pulp as p 
+
 
 def solve_naive(instance: Instance) -> Solution:
     return Solution(
@@ -22,12 +26,76 @@ def solve_naive(instance: Instance) -> Solution:
 
 
 def solve(instance: Instance) -> Solution:
-    return "hi"
     #parsing possible locations
+    possible_loc = set()
+    for city in instance.cities:
+        x = city.x
+        y = city.y
+        possible_loc.union(all_points_in_radius(x,y))
 
-    #three for loops to generate all possible tower placements
+
+
+
+    possible_loc_pts = {}
+    for loc in possible_loc:
+        possible_loc_pts.add(Point(loc[0], loc[1]))
+
+
+
+
+
+
+    #all possible tower placements
+    tower_settings = list(powerset(possible_loc_pts))
 
     #run LP on all the possible solution sets.
+    for tower_setting in tower_settings:
+        #test whether we cover all cities with our towers
+        if not covers_all(tower_setting, instance.cities):
+            continue
+
+        #since we can cover all the cities now we will use these settings to minimize penalty_radius
+        #run LP
+
+
+
+
+
+
+
+
+def covers_all(tower_setting, city_setting):
+    '''checks to see if the towers in tower_setting covers all the cities in city_settings'''
+    all_reachable_from_tower = set()
+
+    for tower in tower_setting:
+        all_reachable_from_tower.union(all_points_in_radius(tower.x, tower.y))
+
+    for city in city_setting:
+        if not (city in all_reachable_from_tower):
+            return False
+
+    return True
+
+
+def all_points_in_radius(x,y):
+    ''' finds all the points in a 3 unit radius around x,y'''
+    possible_loc = set()
+    for i in range(4):
+        for j in range(4):
+            if (sqrt(i^2 + j^2) <= 3):
+                lst = [(x + i,y + j), (x - i, y - j), (x + i, y - j), (x - i, y + j)]
+                for coord in lst:
+                    if (coord[0] >= 0) and (coord[0]< D) and (coord[1]< D) and (coord[1] >= 0):
+                        possible_loc.add(coord)
+
+    return possible_loc
+
+
+
+
+
+
 
 SOLVERS: Dict[str, Callable[[Instance], Solution]] = {
     "naive": solve_naive,
